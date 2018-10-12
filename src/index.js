acdc9950eb5726f2186d1e775a968f4bdd3110e7
Jens-Kristjan <Jens-Kristjan@LAPTOP-92IU9THL.lan>
2018-10-12 15:10:17 +0300

    Added react-intl for later translations.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {IntlProvider} from "react-intl";
 import './index.css';
 //import App from './App';
 import Main from './assets/js/main.jsx';
@@ -15,7 +16,11 @@ import './assets/lib/font-awesome.min.css';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('body'));
+ReactDOM.render(
+    <IntlProvider locale='en'>
+        <Main />
+    </IntlProvider>, 
+    document.getElementById('body'));
 
 
 // ReactDOM.render(<AggregatorPage />, document.getElementById('body'));
