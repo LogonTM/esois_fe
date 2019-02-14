a631983055ad09735e96510d9865cea6f10d4e39
KristiL13 <kristi.laur@mail.ee>
2019-02-14 16:29:20 +0200

    little fix

--- src/assets/js/components/corpus/layeroperator.jsx
+++ src/assets/js/components/corpus/layeroperator.jsx
@@ -8,7 +8,6 @@ const LayerOperator = ({ operator, languageFromMain, checked }) => {
             className="form-check-input"
             type="checkbox"
             value={operator}
-            id={operator}
             checked={checked}
             readOnly
          />
